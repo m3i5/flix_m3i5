30b66dc13a6cf4384f95d1658a218e277066cbbb
m3i5 <jakub.h.zawadzki@gmail.com>
2023-02-14 21:41:23 +0000

    add basic navbar layout

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import App from './components/App';
 
-const container = document.getElementById('app');
-const root = createRoot(container);
+const theme = createTheme( {} )
 
-root.render(<BrowserRouter> <App /> </BrowserRouter>);
+// const container = document.getElementById('app');
+// const root = createRoot(container);
+
+// root.render(<BrowserRouter> <App /> </BrowserRouter>);
+createRoot( document.getElementById( 'app' ) ).render(
+    <ThemeProvider theme={theme}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ThemeProvider>
+);
